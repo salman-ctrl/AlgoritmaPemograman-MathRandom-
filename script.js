0b03a81eac31c52cb0a2b2203f23134e5a9b46a3
salman-ctrl <zirmanvictory@gmail.com>
2024-10-16 00:07:53 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,18 @@
+// setInterval(() => {
+// console.log('tampilkan merah'); 
+// }, 15000);
+
+// setInterval(() => {
+//     console.log('tamplikan kuning');
+// }, 15000);
+
+
+// setInterval(() => {
+//     console.log('hijau');
+// }, 5000);
+
+// const r = Math.floor(Math.random()*225)+1;
+// const g = Math.floor(Math.random()*255)+1;
+// const b = Math.floor(Math.random()*255)+1;
+
+// console.log(`${r},${g},${b}`);
